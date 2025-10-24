045b75f0c70dc577c0ec04953341a1d488a149fc
EtienneDuranton <etienne.duranton.pro@gmail.com>
2025-10-24 23:39:31 +0200

    Déplacements adaptés

--- base/bureau/player_office.gd
+++ base/bureau/player_office.gd
@@ -1,23 +1,20 @@
-extends Area2D
+extends CharacterBody2D
 
-@export var speed = 0
+@export var speed := 200.0
 
-func _ready() -> void:
-	hide()
-	$CollisionShape2D.disabled = true
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("Move_Right"):
-		velocity.x += 1
-	if Input.is_action_pressed("Move_Left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("Move_Up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("Move_Down"):
-		velocity.y += 1	
+	# Normalisation pour que diagonales ne soient pas plus rapides
+	if input_vector != Vector2.ZERO:
+		input_vector = input_vector.normalized()
+
+	velocity = input_vector * speed
+	move_and_slide()  # gère les collisions automatiquement
 	
+	# Animations
 	if velocity.length() > 0:
 		position += velocity.normalized()*speed*delta	
 		$AnimatedSprite2D.play()
@@ -34,10 +31,3 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.animation = "walk_down"
 	elif velocity.y < 0 :
 		$AnimatedSprite2D.animation = "walk_up"	
-	
-	pass
-
-func popPlayer(pPosition):
-	#position = pPosition
-	show()
-	$CollisionShape2D.disabled = false
