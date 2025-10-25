ef006b58f0db4bd3e4c9e977c31627ca4f15376b
EtienneDuranton <etienne.duranton.pro@gmail.com>
2025-10-25 20:18:00 +0200

    WIP quêtes

--- base/bureau/collegue.gd
+++ base/bureau/collegue.gd
@@ -1,9 +1,15 @@
-@tool
 class_name Collegue
 
 extends Area2D
 
 var hasQuest = false
+
+var quetes_table = [
+	QueteData.new("usb","res://minijeux/usb-key/usb_key.tscn","Bubble",true,""),
+	QueteData.new("windows","res://minijeux/restart-windows95/restart_windows95.tscn","Bubble",true,""),
+	QueteData.new("cafe","res://minijeux/machine-a-cafe/machine-a-cafe.tscn","Green",false,"")
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("collegues")	
@@ -15,8 +21,11 @@ func _process(delta: float) -> void:
 	pass
 
 func _popQuest():
+	var lRandQuete = randi_range(0,2)
+	var lQuest:QueteData = quetes_table[lRandQuete]
+	
 	$Exclamation.visible = true
-	$Exclamation.play("Green")
+	$Exclamation.play(lQuest.icon)
 	remove_from_group("collegues")
 	
 	$Declenche_MiniJeu.disabled = false	
