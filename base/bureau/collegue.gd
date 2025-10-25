15fa372e4e96c3c7b77de2c90351fc4a143900dd
EtienneDuranton <etienne.duranton.pro@gmail.com>
2025-10-25 23:08:09 +0200

    Vending icone

--- base/bureau/collegue.gd
+++ base/bureau/collegue.gd
@@ -9,7 +9,8 @@ var quetes_table = [
 	QueteData.new("usb","res://minijeux/usb-key/usb_key.tscn","T_Speech_USB",true,""),
 	QueteData.new("windows","res://minijeux/restart-windows95/restart_windows95.tscn","T_Speech_Windows",true,""),
 	QueteData.new("cafe","res://minijeux/machine-a-cafe/machine-a-cafe.tscn","T_Speech_Coffee",false,""),
-	QueteData.new("car","res://minijeux/parking/parking.tscn","T_Speech_Car",false,"")
+	QueteData.new("car","res://minijeux/parking/parking.tscn","T_Speech_Car",false,""),
+	QueteData.new("vending","res://minijeux/vending-machine/control.tscn","T_Speech_Vending",false,"")
 ]
 
 # Called when the node enters the scene tree for the first time.
