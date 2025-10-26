05b54d58d52ea54be5e6fec07d8e4e86dfc5f272
Martin Delille <martin.delille@gmail.com>
2025-10-26 16:33:15 +0100

    Parking++

--- minijeux/parking/place.gd
+++ minijeux/parking/place.gd
@@ -1,21 +1,29 @@
 extends Area2D
 
-@export var statusColor:Color = Color(0., 1., 0.)
 
+	
 func _process(delta: float) -> void:
 	for body in get_overlapping_bodies():
 		if body is RigidBody2D:
-			$ColorRect.color = check(body)
+			if check(body):
+				$AnimatedSprite2D.hide()
+				return
+			$AnimatedSprite2D.frame = 1
 			return
-		$ColorRect.color = Color(0., 1., 0.)
+	$AnimatedSprite2D.frame = 0
 		
 
 func check(body:RigidBody2D):
 	var body_pos = body.global_position
 	var local_pos = $CollisionShape2D.to_local(body_pos)
-	if local_pos.length()< 10.:
+	var norm_rot = wrapf(body.rotation, -PI, PI)
+	var dist_to_0 = abs(norm_rot)
+	var dist_to_pi = abs(abs(norm_rot) - PI)
+	var min_dist = min(dist_to_0, dist_to_pi)
+
+	if local_pos.length() < 10.:
 		if body.linear_velocity.length() < 0.001:
-			if abs(body.rotation)< 0.1:
-				return Color(1., 0., 0.)
-	return Color(1., 0.5, 0.)
+			if min_dist < 0.1:
+				return true
+	return false
 		
