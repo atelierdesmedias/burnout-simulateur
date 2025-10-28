f3fd7d6ab4438568cfd1412e13e08bd5292f3410
Niels <niels.andre@cnrs.fr>
2025-10-28 10:47:29 +0100

    remise en route de la musique et balance des effets sonores

--- base/bureau/bureaux.gd
+++ base/bureau/bureaux.gd
@@ -1,9 +1,6 @@
-extends Area2D
+extends Node2D
 
-@onready var modem: AudioStreamPlayer = $Modem
 @onready var claviers = [$clavier, $clavier2, $clavier3, $clavier4]
-var player_inside: bool = false
-var saved_position: float = 0.0  # Position de lecture sauvegardée
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,19 +16,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _on_body_entered(body: Node2D) -> void:
-	if body is Player:
-		player_inside = true
-		# Reprendre le son depuis la position actuelle si il était en pause
-		if modem.playing == false:
-			modem.seek(saved_position)
-			modem.play(saved_position)
-
-func _on_body_exited(body: Node2D) -> void:
-	if body is Player:
-		player_inside = false
-		# Mettre le son en pause mais garder la position
-		if modem.playing:
-			saved_position = modem.get_playback_position()  # sauvegarde la position
-			modem.stop()
